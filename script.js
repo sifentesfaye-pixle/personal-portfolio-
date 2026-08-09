e39308ec81a20f6a27d7cbaca1a5485b0c61cb1d
sifentesfaye-pixle <tesfayesifen609@gmail.com>
2026-08-09 12:37:23 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let getToKnowMe = document.getElementsByClassName("getToKnowMe");
+let intro = document.getElementsByClassName("intro");
+
+getToKnowMe[0].addEventListener("click", function(){
+    intro[0].style.opacity = "1";
+});
